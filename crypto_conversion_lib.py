f781027b6e1abe9049164b93a7c3eb1ff24c1273
Christoph Pernul <cpernul@gmail.com>
2021-05-02 19:14:35 +0200

    Add logic for binance.com

--- crypto_conversion_lib.py
+++ crypto_conversion_lib.py
@@ -49,6 +49,7 @@ class Kraken(Exchange):
         self.deposits["date_string"] = self.deposits["date"].dt.strftime('%Y-%m-%d')
         self.deposits["fee_currency"] = np.nan
 
+
     def convert_trades(self):
         """
         BUYS: First currency in pair is the currency, that is received, second one is spent
@@ -173,3 +174,113 @@ class Kucoin(Exchange):
 
 
 
+class Binance(Exchange):
+    def __init__(self):
+        Exchange.__init__(self)
+        # Load ledger and trades data for kraken.com
+        (self.deposits_input, self.trades_input) = self.data_initialization()
+    def data_initialization(self):
+        filenames_deposits = list(filter(lambda fname: "binance_deposits" in fname, os.listdir(self.raw_path)))
+        filenames_trades = list(filter(lambda fname: "binance_tradehistory" in fname, os.listdir(self.raw_path)))
+        for counter, fname in enumerate(filenames_deposits):
+            try:
+                # Use xlrd library to load this type of excel xlsx path, but be aware that an old library 1.2.0 is used
+                # since in newer versions this is not possible anymore.
+                # Switch to openpyxl once this bug is solved: https://github.com/pandas-dev/pandas/issues/39001
+                df = pd.read_excel(os.path.join(self.raw_path, fname))
+                if counter == 0:
+                    deposits_init = df
+                else:
+                    deposits_init = pd.concat([deposits_init, df], ignore_index=True, sort=False)
+            except pd.errors.EmptyDataError:
+                print(pd.errors.EmptyDataError, " for ", fname)
+
+        for counter, fname in enumerate(filenames_trades):
+            try:
+                dft = pd.read_csv(os.path.join(self.raw_path, fname))
+                if counter == 0:
+                    trades_init = dft
+                else:
+                    trades_init = pd.concat([trades_init, dft], ignore_index=True, sort=False)
+            except pd.errors.EmptyDataError:
+                print(pd.errors.EmptyDataError, " for ", fname)
+
+        return ((deposits_init, trades_init))
+
+    def convert_deposits(self):
+        self.deposits = self.deposits_input.rename(columns={"Date(UTC)": "date",
+                                                                  "Coin": "currency_received",
+                                                                  "Amount": "amount_received",
+                                                                  "TransactionFee": "fee",
+                                                                  "TXID": "txid",
+                                                                  "PaymentID": "ordertxid"}
+                                                       ).drop(["Status", "SourceAddress", "Address"], axis=1)
+        self.deposits["type"] = "deposit"
+        # self.deposits["balance"] = np.nan
+        # self.deposits["currency_spent"] = np.nan
+        # self.deposits["amount_spent"] = np.nan
+        # self.deposits["fee_currency"] = np.nan
+        self.deposits["date"] = pd.to_datetime(self.deposits["date"], format='%Y-%m-%d %H:%M:%S')
+        self.deposits["date_string"] = self.deposits["date"].dt.strftime('%Y-%m-%d')
+        self.deposits["fee"] = self.deposits["fee"].astype(float)
+        self.deposits["ordertxid"] = self.deposits["ordertxid"].astype(str)
+
+    def convert_trades(self):
+        """
+        BUYS: First currency in pair is the currency, that is received, second one is spent
+        SELLS: First currency in pair is the currency, that is spent, second one is received
+        Fee is paid in currency one receives with (example: buy BTC with EOS, fee is paid in BTC)
+        currency_spent = conversion_rate * currency_gained
+        :return:
+        """
+        buys = self.trades_input[self.trades_input["Side"] == "BUY"].copy()
+        buys["currency_received"] = buys["Pair"].apply(lambda pair: pair[:len(pair) // 2])
+        buys["currency_spent"] = buys["Pair"].apply(lambda pair: pair[len(pair) // 2:])
+        buys = buys.rename(columns={"Date(UTC)": "date",
+                                    "Side": "type",
+                                    "Amount": "amount_spent",
+                                    "Executed": "amount_received",
+                                    "Fee": "fee",
+                                    "Price": "conversion_rate_received_spent"
+                                    }).drop(["Pair"], axis=1)
+        buys["type"] = buys["type"].apply(lambda string: string.lower())
+        buys["amount_received"] = buys["amount_received"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        buys["amount_spent"] = buys["amount_spent"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        buys["fee_currency"] = buys["fee"].str.extract("([a-zA-Z]+)")
+        buys["fee"] = buys["fee"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        buys["margin"] = 0.
+        buys["balance"] = np.nan
+        buys["txid"] = np.nan
+        buys["ordertxid"] = np.nan
+        buys["ordertype"] = "market"
+
+
+        sells = self.trades_input[self.trades_input["Side"] == "SELL"].copy()
+        sells["currency_spent"] = sells["Pair"].apply(lambda pair: pair[:len(pair) // 2])
+        sells["currency_received"] = sells["Pair"].apply(lambda pair: pair[len(pair) // 2:])
+        sells = sells.rename(columns={"Date(UTC)": "date",
+                                      "Side": "type",
+                                      "Amount": "amount_received",
+                                      "Executed": "amount_spent",
+                                      "Fee": "fee",
+                                      "Price": "conversion_rate_received_spent"
+                                      }).drop(["Pair"], axis=1)
+        sells["conversion_rate_received_spent"] = 1. / sells["conversion_rate_received_spent"]
+        sells["type"] = sells["type"].apply(lambda string: string.lower())
+        sells["amount_received"] = sells["amount_received"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        sells["amount_spent"] = sells["amount_spent"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        sells["fee_currency"] = sells["fee"].str.extract("([a-zA-Z]+)")
+        sells["fee"] = sells["fee"].str.replace(",", "").str.replace("[a-zA-Z]+", "")
+        sells["margin"] = 0.
+        sells["balance"] = np.nan
+        sells["txid"] = np.nan
+        sells["ordertxid"] = np.nan
+        sells["ordertype"] = "market"
+
+        # Combine BUY and SELL into single table
+        self.trades = pd.concat([buys, sells], ignore_index=True)
+        self.trades["date"] = pd.to_datetime(self.trades["date"], format='%Y-%m-%d %H:%M:%S')
+        self.trades["date_string"] = self.trades["date"].dt.strftime('%Y-%m-%d')
+        self.trades["ordertxid"] = self.trades["ordertxid"].astype(str)
+        self.trades["fee"] = self.trades["fee"].astype(float)
+        self.trades["fee"] *= -1
